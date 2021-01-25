ce496ff91d9e2e55f8b702a3cdb681de88f1d509
Mateus Romão <m.romao.ds@gmail.com>
2021-01-25 17:50:15 -0300

    m

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,7 @@ import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
+import Head from '../src/components/Head'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -26,28 +20,31 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <>
+    <Head/>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
-    </QuizBackground>
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      </QuizBackground>
+    </>
   );
 }
---
+++ src/components/Head/index.js
@@ -0,0 +1,16 @@
+import { Component } from "react"
+
+export default class extends Component {
+  render() {
+    return (
+      <>
+        <head>
+            <title>Poke-Quiz</title>
+            <meta property="og:image" content="www.meusite.com.br/imhttps://i.pinimg.com/originals/f2/b8/5f/f2b85ff3b6c18f8ba352429d7bdc4de6.jpg?epik=dj0yJnU9UTRVM21SM2g5ME11dTdPSWRLT2JNVjdYZHNMX1JhTmMmcD0wJm49aVRTTGxkbzQ0MUVDVllrcjBnSXVXdyZ0PUFBQUFBR0FQSFhZagem.jpg"/>
+            <meta property="og:image:width" content="800"/>
+            <meta property="og:image:height" content="600"/>
+        </head>
+      </>
+    )
+  }
+}
