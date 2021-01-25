250f8b7a9698a6b216881cce5d8d39fe1d997a11
Mateus Romão <m.romao.ds@gmail.com>
2021-01-25 18:42:32 -0300

    arrumando o tamanho

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -7,6 +7,8 @@ export default class extends Component {
         <head>
             <title>Pokequiz</title>
             <meta property="og:image" content="https://i.pinimg.com/originals/f2/b8/5f/f2b85ff3b6c18f8ba352429d7bdc4de6.jpg?epik=dj0yJnU9UTRVM21SM2g5ME11dTdPSWRLT2JNVjdYZHNMX1JhTmMmcD0wJm49aVRTTGxkbzQ0MUVDVllrcjBnSXVXdyZ0PUFBQUFBR0FQSFhZ"/>
+            <meta property="og:image:width" content="800"/>
+            <meta property="og:image:height" content="600"/>
             <meta property="og:title" content="Pokequiz."/>
             <meta property="og:description" content="Teste seus conhecimentos pokemon com esse quiz."></meta>
         </head>
